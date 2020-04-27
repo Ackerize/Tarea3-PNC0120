e168122295a86edc692d1fdff5ebc23183c9e558
DavidVallecios <00078217@uca.edu.sv>
2020-04-26 21:29:24 -0600

    [Tarea3] Finalizada

---
+++ tarea3/src/main/java/com/uca/capas/tarea3/controller/MainController.java
@@ -0,0 +1,82 @@
+package com.uca.capas.tarea3.controller;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/ingresar")
+	public ModelAndView ingresar() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("ingreso");
+		return mav;
+	}
+
+	@RequestMapping("/alumno")
+	public ModelAndView alumno(
+			@RequestParam String nombres, 
+			@RequestParam String apellidos,
+			@RequestParam String fechaNacimiento,
+			@RequestParam String lugarNacimiento,
+			@RequestParam String colegio,
+			@RequestParam String telFijo,
+			@RequestParam String telMovil) throws ParseException {
+		
+		ModelAndView mav = new ModelAndView();
+		
+		ArrayList<String> errores = new ArrayList<String>();
+		
+		if(!(nombres.length() >= 1 && nombres.length() <= 25)) {
+			errores.add("*- La longitud del nombre debe ser mayor a 1 y menor a 25");
+		}
+		if(!(apellidos.length() >= 1 && apellidos.length() <= 25)) {
+			errores.add("*- La longitud del apellido debe ser mayor a 1 y menor a 25");
+		}
+		if(validarFecha(fechaNacimiento)){
+			errores.add("*- La fecha de nacimiento debe ser mayor o igual al 1 de enero del 2003");
+		}
+		if(!(lugarNacimiento.length() >= 1 && lugarNacimiento.length() <= 25)) {
+			errores.add("*- La longitud del lugar de nacimiento debe ser mayor a 1 y menor a 25");
+		}
+		if(!(colegio.length() >= 1 && colegio.length() <= 100)) {
+			errores.add("*- La longitud del colegio debe ser mayor a 1 y menor a 25");
+		}
+		if(telFijo.length() != 8) {
+			errores.add("*- El telefono fijo debe tener estrictamente 8 digitos");
+		}
+		if(telMovil.length() != 8) {
+			errores.add("*- El telefono movil debe tener estrictamente 8 digitos");
+		}
+		
+		if(errores.isEmpty()){
+			mav.setViewName("Acceso");
+		}else {
+			mav.addObject("req", errores);
+			mav.setViewName("Denegado");
+			}
+			return mav;
+		}
+
+			
+	public boolean validarFecha(String fechaNacimiento) throws ParseException {
+		String fechaMinima = "2003-01-01";
+		SimpleDateFormat formateador = new SimpleDateFormat("yyyy-MM-dd"); 
+		
+		Date fecha1 = formateador.parse(fechaNacimiento);
+		Date fecha2 = formateador.parse(fechaMinima);
+		
+		if(fecha1.before(fecha2)) {
+			return true;
+		}else {
+			return false;
+		}  
+	}
+}
